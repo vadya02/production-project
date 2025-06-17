f1f8711b44efc0b7cf8dbabf6c97b91b0591fdd9
vadya02 <114842460+vadya02@users.noreply.github.com>
2025-06-17 21:40:54 +0800

    62. CopyPlugin. Подготовка к продакшну.

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.stories.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.stories.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 
-import { ArticleEditPage } from './ArticleEditPage';
+import ArticleEditPage from "./ArticleEditPage";
 
 export default {
-    title: 'shared/ArticleEditPage',
-    component: ArticleEditPage,
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
+	title: "shared/ArticleEditPage",
+	component: ArticleEditPage,
+	argTypes: {
+		backgroundColor: { control: "color" },
+	},
 } as ComponentMeta<typeof ArticleEditPage>;
 
-const Template: ComponentStory<typeof ArticleEditPage> = (args) => <ArticleEditPage {...args} />;
+const Template: ComponentStory<typeof ArticleEditPage> = (args) => (
+	<ArticleEditPage {...args} />
+);
 
 export const Normal = Template.bind({});
 Normal.args = {};
