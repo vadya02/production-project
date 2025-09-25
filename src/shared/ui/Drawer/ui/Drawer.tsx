660cfae75901eaf70f1b642d95727c490c85665b
vadim <vadin@MacBook-Pro-vadin.local>
2025-09-25 19:19:30 +0800

    84 BrowserList. Размер бандла. Исправляем Drawer animation provider

--- src/shared/ui/Drawer/ui/Drawer.tsx
+++ src/shared/ui/Drawer/ui/Drawer.tsx
@@ -3,7 +3,7 @@ import {
 } from 'react';
 import { useTheme } from '@/app/providers/ThemeProvider';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { useAnimationLibs } from '@/shared/lib/components/AnimationProvider/AnimationProvider';
+import { AnimationProvider, useAnimationLibs } from '@/shared/lib/components/AnimationProvider/AnimationProvider';
 import { Overlay } from '../../Overlay/ui/Overlay';
 import { Portal } from '../../Portal/Portal';
 import cls from './Drawer.module.scss';
@@ -91,7 +91,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
     );
 });
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = memo((props: DrawerProps) => {
     const { isLoaded } = useAnimationLibs();
 
     if (!isLoaded) {
@@ -100,3 +100,12 @@ export const Drawer = memo((props: DrawerProps) => {
 
     return <DrawerContent {...props} />;
 });
+
+
+export const Drawer = (props: DrawerProps) => {
+    return (
+        <AnimationProvider>
+            <DrawerAsync {...props}/>
+        </AnimationProvider>
+    )
+}
