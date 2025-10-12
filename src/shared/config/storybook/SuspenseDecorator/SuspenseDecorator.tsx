6864e5960e56a89b8694503999b688510d0d7d6b
vadim <vadin@MacBook-Pro-vadin.local>
2025-10-12 22:44:02 +0800

    99. Storybook addon theme

--- src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator.tsx
+++ src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator.tsx
@@ -1,8 +1,8 @@
 import { Story } from '@storybook/react';
 import { Suspense } from 'react';
 
-export const SuspenseDecorator = (StoryComponent: Story) => {
+export const SuspenseDecorator = (StoryComponent: Story) => (
     <Suspense>
         <StoryComponent />
-    </Suspense>;
-};
+    </Suspense>
+);
