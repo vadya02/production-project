f5ee9ada889160c02c761b72f6e9d9923b8e30d4
vadim <vadin@MacBook-Pro-vadin.local>
2025-10-13 19:46:30 +0800

    101. Однозначное сопоставление типов в ReducersList

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -3,6 +3,7 @@ import { ReactNode, useEffect } from 'react';
 import { useDispatch, useStore } from 'react-redux';
 import { ReduxStoreWithManager, StateSchema, StateSchemakey } from '@/app/providers/StoreProvider/config/StateSchema';
 
+
 export type ReducersList = {
     [name in StateSchemakey]?: Reducer<NonNullable<StateSchema[name]>>;
 };
